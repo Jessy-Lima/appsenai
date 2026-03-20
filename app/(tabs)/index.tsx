ccfc566666b69e6aa31b899e493dc3a33339dcae
Jessica Lima <maytajessica73@gmail.com>
2026-03-20 11:25:36 -0300

    salvando

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+
+    <View style={styles.container} >
+
+      <Text style={styles.title}>Meu Primeiro app no Celular</Text>
+
+      <Text> React Native </Text>
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff"
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#000000"
+  },
+
+});
